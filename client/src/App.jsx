33e6e5355eb0ac15ba47092582f9e81650a20512
Ganesh Lekurwale <69107680+Ganeshlekurwale@users.noreply.github.com>
2024-12-28 00:53:43 +0530

    add basename to router configuration and simplify main rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,20 @@ import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
+const appRouter = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ],
+  { basename: "/Todo-V1.2" } // <-- Add the basename here
+);
+
 function App() {
   return <RouterProvider router={appRouter} />;
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Todo-V1.2">
-      <App />
-      <div>
-        <Toaster />
-      </div>
-    </BrowserRouter>
+    <App />
+    <div>
+      <Toaster />
+    </div>
   </StrictMode>
 );
