d2f938055b71063cdc74a50afd8e466e02b71dd6
Ganesh Lekurwale <69107680+Ganeshlekurwale@users.noreply.github.com>
2024-12-28 00:44:31 +0530

    wrap App component with BrowserRouter for routing support

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <div>
-      <Toaster />
-    </div>
+    <BrowserRouter basename="/Todo-V1.2">
+      <App />
+      <div>
+        <Toaster />
+      </div>
+    </BrowserRouter>
   </StrictMode>
 );
